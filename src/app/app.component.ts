9c8b6bba0b90137fce2350b1467a9dede8ceb78e
Jhonathan Ruiz <Jhonathansr@arqs.co>
2024-01-10 01:19:30 -0500

    ajuste de proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MaterialModule} from "./material.module";
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ RouterOutlet,MaterialModule,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private matIconRegistry = inject(MatIconRegistry);
+  private domSanitizer= inject(DomSanitizer);
   title = 'frontend-inventory';
+
+  constructor(){
+    this.matIconRegistry.addSvgIcon(
+      "menuicon",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/menuicon.svg")
+    );
+  }
+  ngOnInit(): void {}
 }

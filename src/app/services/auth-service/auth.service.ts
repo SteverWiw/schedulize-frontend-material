d13e46cad84c6c0296bd381c5e6be3c794826d2d
Jhonathan Ruiz <Jhonathansr@arqs.co>
2024-01-11 01:59:08 -0500

    se realiza versionamiento para el manejo de session y el login desde backend, se deja versionado el proxy en caso de ser necesitado en otro entorno de desarrollo

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,13 +1,38 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {Authrequest} from "./authrequest";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {catchError, map, Observable, tap, throwError} from "rxjs";
+import {TokenService} from "./TokenService";
+
+const base_url = "http://localhost:8090/app-jwt-auth/v1/api/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private token: string | null = '';
+  private http = inject(HttpClient);
+  private tokenService = inject(TokenService);
+  login(authrequest: Authrequest){
+    const body = {
+      userName: authrequest.userName,
+      password: authrequest.password
+    } as Authrequest;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    const path = `${base_url}/login`;
+
+    return this.http.post<any>(path, body, { headers: headers }).pipe(
+      tap(response => {
+        this.tokenService.setToken(response.objectList[0].token);
+      })
+    );
+  }
 
   isAuth(): boolean {
-    return this.token != null && this.token.length > 0? true:false;
+    const expirationDate = this.tokenService.getExpirationDate();
+    return expirationDate !== null && expirationDate > new Date();
   }
 
 }
---
+++ src/app/services/auth-service/authrequest.ts
@@ -0,0 +1,4 @@
+export interface Authrequest {
+  password: string;
+  userName: string;
+}
